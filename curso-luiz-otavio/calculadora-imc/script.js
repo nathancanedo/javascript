69f27681e0b848c54f1c88b94d692f499dd7e8e3
Nathan Canedo <101233593+nathancanedo@users.noreply.github.com>
2022-07-01 15:07:21 -0300

    Update script.js

--- curso-luiz-otavio/calculadora-imc/script.js
+++ curso-luiz-otavio/calculadora-imc/script.js
@@ -6,5 +6,30 @@ let resultado = document.querySelector('#resultado');
 
 function calcular() {
     const imc = (peso.value / (altura.value * altura.value)).toFixed('1');
-    resultado.innerHTML = `Olá ${nome.value} seu IMC é ${imc}`;
+    resultado.innerHTML = `Olá ${nome.value}! Seu IMC é ${imc}`;
+
+    if (idade.value < 60) {
+        if (imc <= 18.4) {
+            resultado.innerHTML += '<p>Você está com <strong>déficit de massa corporal</strong>. Procure um nutricionista!</p>';
+        }
+        else if (imc < 25) {
+            resultado.innerHTML += '<p>Seu IMC está <strong>normal</strong>. Continue mantendo hábitos saudáveis e visite o médico regularmente.';
+        }
+
+        else if (imc < 30) {
+            resultado.innerHTML += '<p>Você está com <strong>sobrepeso</strong>. Procure um nutricionista!</p>';
+        }
+        
+        else if (imc < 35) {
+            resultado.innerHTML += '<p>Você está com <strong>obesidade leve</strong>. Procure um nutricionista!</p>';
+        }
+
+        else if (imc < 40) {
+            resultado.innerHTML += '<p>Você está com <strong>obesidade média</strong>. Procure um nutricionista!</p>';
+        }
+
+        else if (imc >= 40) {
+            resultado.innerHTML += '<p>Você está com <strong>obesidade mórbida</strong>. Procure um nutricionista!</p>';
+        }
+    }
 }
