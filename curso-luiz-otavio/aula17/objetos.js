042c994e9642d523373d4263ce44542b7e302864
Nathan Canedo <101233593+nathancanedo@users.noreply.github.com>
2022-07-04 16:00:31 -0300

    exercicios atualizados

--- curso-luiz-otavio/aula17/objetos.js
+++ curso-luiz-otavio/aula17/objetos.js
@@ -1,1 +1,17 @@
-pessoa 
+function criaPessoa (nome, sobrenome, idade) {
+    return { nome, sobrenome, idade }; 
+    /* 
+    nome = nome
+    sobrenome = sobrenome
+    idade = idade
+    */
+}
+
+const pessoa1 = criaPessoa('Carlos', 'Mendonça', 35);
+const pessoa2 = criaPessoa('Lia', 'Souza', 65);
+const pessoa3 = criaPessoa('Lucas', 'Soares', 45);
+const pessoa4 = criaPessoa('Ricardo', 'Silveira', 25);
+const pessoa5 = criaPessoa('Mauricio', 'Lima', 54);
+
+console.log(pessoa1.nome, pessoa5.nome);
+
