ac9e0b177e4ee1c1f69e85b6b08a37581431277c
Nathan Canedo <101233593+nathancanedo@users.noreply.github.com>
2022-07-08 15:44:37 -0300

    testando API

---
+++ exercicios-avulsos/conversor-de-moedas/script/script.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const moedas = 'USD-BRL,BTC-USD,BRL-USD';
+
+const option = {
+    url: `https://economia.awesomeapi.com.br/last/${moedas}`,
+    method: 'GET',
+    Headers: {
+        'Accept': 'application/json', 
+        'Accept-Charset': 'utf-8'
+    }
+}
+
+const callbackDolar = function(erro, res, body){
+    let json = JSON.parse(body);
+    const cotacaoDolar = json.USDBRL['bid'];
+    console.log(cotacaoDolar);
+}
+
+request(option, callbackDolar);
+
+
