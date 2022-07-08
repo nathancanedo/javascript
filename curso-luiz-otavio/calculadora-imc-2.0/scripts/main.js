82e23eb4517cd727bc5be2b10066e1660a1416ed
Nathan Canedo <101233593+nathancanedo@users.noreply.github.com>
2022-07-07 21:50:09 -0300

    exercício atualizado

--- curso-luiz-otavio/calculadora-imc-2.0/scripts/main.js
+++ curso-luiz-otavio/calculadora-imc-2.0/scripts/main.js
@@ -1,4 +1,8 @@
 function pegarDados() {
+    const form = document.querySelector('#form');
+    form.addEventListener('submit', event => {event.preventDefault();});
+
+
     const peso = Number(document.querySelector('#peso').value);
     const altura = Number(document.querySelector('#altura').value);
     
@@ -12,18 +16,25 @@ function pegarDados() {
     
     function resultado(imc) {
         const resultado = document.querySelector('#resultado');
-        if (imc < 18.5) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Abaixo do peso)`;
+
+        if ((peso <= 0 && altura <= 0) || (isNaN(peso) && isNaN(altura)) || (isNaN(peso) && altura <= 0) || (peso <=0 && isNaN(altura))) {
+            resultado.innerHTML = '<p class="vermelho">Peso e Altura inválidos!</p>';
+        } else if (peso <= 0 || isNaN(peso)) { 
+            resultado.innerHTML = '<p class="vermelho">Peso Inválido!</p>';
+        } else if (altura <= 0 || isNaN(altura)) {
+            resultado.innerHTML = '<p class="vermelho">Altura Inválida!</p>';
+        } else if (imc < 18.5) {
+            resultado.innerHTML = `<p class="verde">Seu IMC é ${imc} (Abaixo do peso)</p>`;
         } else if (imc < 25) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Peso normal)`;
+            resultado.innerHTML = `<p class="verde">Seu IMC é ${imc} (Peso normal)</p>`;
         } else if (imc < 30) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Sobrepeso)`;
+            resultado.innerHTML = `<p class="verde">Seu IMC é ${imc} (Sobrepeso)</p>`;
         } else if (imc < 35) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Obesidade grau 1)`;
+            resultado.innerHTML = `<p class="verde">Seu IMC é ${imc} (Obesidade grau 1)</p>`;
         } else if (imc < 40) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Obesidade grau 2)`;
+            resultado.innerHTML = `<p class="verde">Seu IMC é ${imc} (Obesidade grau 2)</p>`;
         } else if (imc >= 40) {
-            resultado.innerHTML = `Seu IMC é ${imc} (Obesidade grau 3)`;
+            resultado.innerHTML = `<p class="verde">Seu IMC é ${imc} (Obesidade grau 3)</p>`;
         }
     }
 }
