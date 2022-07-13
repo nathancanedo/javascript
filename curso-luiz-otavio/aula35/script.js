8413e5ec9077532b3acd8b67780f398ff588c3c9
Nathan Canedo <101233593+nathancanedo@users.noreply.github.com>
2022-07-13 01:42:09 -0300

    Create script.js

---
+++ curso-luiz-otavio/aula35/script.js
@@ -0,0 +1,14 @@
+let [a, b, c] = ['A', 'B' , 'C'];
+
+const nomes = ['Luiz', 'Henrique', 'Xavier', 'Navarro'];
+
+const [nome1, nome2, nome3, nome4] = nomes
+
+[a, b, c] = [b, c, a];
+
+console.log(a, b, c);
+
+console.log (nome2);
+
+
+console.log(nome2);
