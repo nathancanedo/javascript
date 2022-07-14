71e3060b7a16029df0fd140d4c1b0a3c84b71912
Nathan Canedo <101233593+nathancanedo@users.noreply.github.com>
2022-07-13 23:57:23 -0300

    Update script.js

--- curso-luiz-otavio/aula35/script.js
+++ curso-luiz-otavio/aula35/script.js
@@ -4,11 +4,12 @@ const nomes = ['Luiz', 'Henrique', 'Xavier', 'Navarro'];
 
 const [nome1, nome2, nome3, nome4] = nomes
 
-[a, b, c] = [b, c, a];
+
 
 console.log(a, b, c);
 
 console.log (nome2);
 
+[a, b, c] = [b, c, a];
 
 console.log(nome2);
